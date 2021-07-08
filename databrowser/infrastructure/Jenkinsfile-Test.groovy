dc93881f1237ea9a7e0e8c191f2b3b404a235498
gappc <gapp.christian@gmail.com>
2021-07-08 16:22:26 +0200

    Add Jenkins files

---
+++ databrowser/infrastructure/Jenkinsfile-Test.groovy
@@ -0,0 +1,42 @@
+pipeline {
+    agent {
+        dockerfile {
+            filename 'infrastructure/docker/dockerfile-node'
+            additionalBuildArgs '--build-arg JENKINS_USER_ID=`id -u jenkins` --build-arg JENKINS_GROUP_ID=`id -g jenkins`'
+        }
+    }
+
+    environment {
+        AWS_ACCESS_KEY_ID = credentials('AWS_ACCESS_KEY_ID')
+        AWS_SECRET_ACCESS_KEY = credentials('AWS_SECRET_ACCESS_KEY')
+
+        KEYCLOAK_CLIENT_ID = "odh-databrowser"
+        KEYCLOAK_AUTHORIZATION_URI = "https://auth.opendatahub.testingmachine.eu/auth/realms/noi/protocol/openid-connect/auth"
+        KEYCLOAK_TOKEN_URI = "https://auth.opendatahub.testingmachine.eu/auth/realms/noi/protocol/openid-connect/token"
+        KEYCLOAK_USERINFO_URI = "https://auth.opendatahub.testingmachine.eu/auth/realms/noi/protocol/openid-connect/userinfo"
+        KEYCLOAK_LOGOUT_URI = "https://auth.opendatahub.testingmachine.eu/auth/realms/noi/protocol/openid-connect/logout?redirect_uri=http://localhost:3000"
+    }
+
+    stages {
+        stage('Dependencies') {
+            steps {
+                sh 'yarn install'
+            }
+        }
+        stage('Test') {
+            steps {
+                sh 'yarn run test'
+            }
+        }
+        stage('Build') {
+            steps {
+                sh 'yarn run generate'
+            }
+        }
+        stage('Upload') {
+            steps {
+                s3Upload(bucket: 'test-databrowser', acl: 'PublicRead', file: './dist')
+            }
+        }
+    }
+}
