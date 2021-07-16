502f8e09d6c4671665e9669475676952d1229a37
gappc <gapp.christian@gmail.com>
2021-07-17 00:03:34 +0200

    Implement Vue directive to pass data as property to elements
    Setting data through properties allows to pass all kind of data
    (objects, functions, ...) by reference to the element.
    
    Note that the target element needs to handle property changes
    accordingly.

--- databrowser/nuxt.config.js
+++ databrowser/nuxt.config.js
@@ -20,7 +20,10 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['~/plugins/web-component.import.ts'],
+  plugins: [
+    { src: '~/plugins/passref.directive.ts' },
+    { src: '~/plugins/web-component.import.ts' },
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
