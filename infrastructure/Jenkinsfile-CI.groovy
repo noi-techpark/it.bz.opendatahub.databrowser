38c879da4f98b6e10f4990eee246ac1ee8f5be35
Alex Lanz <alex.lanz@aboutbits.it>
2021-07-19 14:06:51 +0200

    Deplyoment

---
+++ infrastructure/Jenkinsfile-CI.groovy
@@ -0,0 +1,26 @@
+pipeline {
+    agent {
+        dockerfile {
+            filename 'infrastructure/docker/dockerfile-node'
+            additionalBuildArgs '--build-arg JENKINS_USER_ID=`id -u jenkins` --build-arg JENKINS_GROUP_ID=`id -g jenkins`'
+        }
+    }
+
+    stages {
+        stage('Dependencies') {
+            steps {
+                sh 'npm run bootstrap'
+            }
+        }
+        stage('Test') {
+            steps {
+                sh 'cd databrowser && npm run test'
+            }
+        }
+        stage('Build') {
+            steps {
+                sh 'cd databrowser && npm run build'
+            }
+        }
+    }
+}
---
+++ infrastructure/Jenkinsfile-Test.groovy
@@ -0,0 +1,36 @@
+pipeline {
+    agent {
+        dockerfile {
+            filename 'infrastructure/docker/dockerfile-node'
+            additionalBuildArgs '--build-arg JENKINS_USER_ID=`id -u jenkins` --build-arg JENKINS_GROUP_ID=`id -g jenkins`'
+        }
+    }
+
+    environment {
+        AWS_ACCESS_KEY_ID = credentials('AWS_ACCESS_KEY_ID')
+        AWS_SECRET_ACCESS_KEY = credentials('AWS_SECRET_ACCESS_KEY')
+    }
+
+    stages {
+        stage('Dependencies') {
+            steps {
+                sh 'npm run bootstrap'
+            }
+        }
+        stage('Test') {
+            steps {
+                sh 'cd databrowser && npm run test'
+            }
+        }
+        stage('Build') {
+            steps {
+                sh 'cd databrowser && npm run build'
+            }
+        }
+        stage('Upload') {
+            steps {
+                s3Upload(bucket: 'it.bz.opendatahub.databrowser-test', acl: 'PublicRead', file: './databrowser/dist')
+            }
+        }
+    }
+}
