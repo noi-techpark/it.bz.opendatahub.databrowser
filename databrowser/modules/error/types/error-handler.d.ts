1c5330df8b9da5a444d33b364d27ee696c748b34
gappc <gapp.christian@gmail.com>
2021-11-11 14:29:03 +0100

    Delete old databrowser application
    This step is a preparation step to rebuild the application on top of
    nuxt3. In this step, the old files get deleted. If necessary, they can
    be restored from git history.

--- databrowser/modules/error/types/error-handler.d.ts
+++
@@ -1,7 +0,0 @@
-import { ErrorHandler } from '~/modules/error/plugins/error-handler';
-
-declare module 'vue/types/vue' {
-  interface Vue {
-    $errorHandler: ErrorHandler;
-  }
-}
--- databrowser/modules/generic-renderer/config/renderer.enum.ts
+++
@@ -1,5 +0,0 @@
-export enum GenericRendererElement {
-  LINK = 'databrowser-render-link',
-  JSON = 'databrowser-render-json',
-  STRING = 'databrowser-render-string',
-}
