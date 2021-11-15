4a2076c28c73dcdff73c167a685f2e7679ac7b55
gappc <gapp.christian@gmail.com>
2021-11-15 22:40:07 +0100

    Add vue-query to the project
    The vue-query package provides hooks for fetching, caching and
    updating asynchronous data in Vue.

--- databrowser/src/App.vue
+++ databrowser/src/App.vue
@@ -8,8 +8,21 @@
   <AppLayout>
     <router-view></router-view>
   </AppLayout>
+  <VueQueryDevTools />
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
+import { defineComponent } from '@vue/runtime-core';
+import { useQueryProvider } from 'vue-query';
+import { VueQueryDevTools } from 'vue-query/devtools';
+
 import AppLayout from './layouts/AppLayout.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: { AppLayout, VueQueryDevTools },
+  setup() {
+    useQueryProvider();
+  },
+});
 </script>
--- databrowser/src/pages/AxiosExample.vue
+++ databrowser/src/pages/AxiosExample.vue
@@ -1,6 +1,21 @@
 <template>
   <h1 class="text-2xl">Axios Page</h1>
-  <p>This component shows the usage of axios</p>
+  <p>
+    This component shows the usage of
+    <a
+      href="https://www.npmjs.com/package/vue-axios"
+      class="underline"
+      target="_blank"
+      >axios</a
+    >
+    and
+    <a
+      href="https://www.npmjs.com/package/vue-query"
+      class="underline"
+      target="_blank"
+      >vue-query</a
+    >
+  </p>
   <div>
     <button
       type="button"
@@ -12,29 +27,36 @@
         px-4
         rounded
       "
-      @click="fetchMobility"
+      @click="refetch()"
     >
       Fetch OpenAPI description for the mobility dataset
     </button>
   </div>
-  <pre>{{ openApiData?.data }}</pre>
+  <div v-if="isLoading">Loading...</div>
+  <div v-else-if="isError">An error has occurred: {{ error }}</div>
+  <div v-else-if="data">
+    <pre>{{ data }}</pre>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/runtime-core';
+import { defineComponent, inject } from '@vue/runtime-core';
+import { useQuery } from 'vue-query';
 
 export default defineComponent({
-  data() {
-    return {
-      openApiData: null as unknown as { data: string },
-    };
-  },
-  methods: {
-    async fetchMobility() {
-      this.openApiData = await this.axios.get(
-        'https://mobility.api.opendatahub.bz.it/v2/apispec'
-      );
-    },
+  setup() {
+    const axios: any = inject('axios');
+
+    const fetcher = async (): Promise<string> =>
+      await axios.get('https://mobility.api.opendatahub.bz.it/v2/apispec');
+
+    const { isLoading, isError, isFetching, data, error, refetch } = useQuery(
+      'mobilityOpenApi',
+      fetcher,
+      { enabled: false }
+    );
+
+    return { isLoading, isError, isFetching, data, error, refetch };
   },
 });
 </script>
