bce8b6830f0733b61c9b9351ec89989c00a41854
Martin Malfertheiner <martin.malfertheiner@aboutbits.it>
2021-11-23 14:24:43 +0100

    merge and move user-authentication into app header

--- databrowser/src/components/header/AppHeader.vue
+++ databrowser/src/components/header/AppHeader.vue
@@ -2,15 +2,17 @@
   <header>
     <router-link to="/" class="text-2xl">OPENDATAHUB!</router-link>
     <LanguageSwitcher></LanguageSwitcher>
+    <user-authentication />
   </header>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@vue/runtime-core';
 import LanguageSwitcher from '../language-switcher/LanguageSwitcher.vue';
+import UserAuthentication from '../../domain/auth/UserAuthentication.vue';
 
 export default defineComponent({
-  components: { LanguageSwitcher },
+  components: { UserAuthentication, LanguageSwitcher },
 });
 </script>
 
--- databrowser/src/layouts/AppLayoutDefault.vue
+++ databrowser/src/layouts/AppLayoutDefault.vue
@@ -2,8 +2,6 @@
   <div class="flex flex-col h-full">
     <AppHeader />
 
-    <user-authentication />
-
     <main class="overflow-auto flex-auto h-full">
       <router-view></router-view>
     </main>
@@ -16,9 +14,8 @@
 import { defineComponent } from '@vue/runtime-core';
 import AppHeader from '../components/header/AppHeader.vue';
 import AppFooter from '../components/footer/AppFooter.vue';
-import UserAuthentication from '../domain/auth/UserAuthentication.vue';
 
 export default defineComponent({
-  components: { AppHeader, AppFooter, UserAuthentication },
+  components: { AppHeader, AppFooter },
 });
 </script>
