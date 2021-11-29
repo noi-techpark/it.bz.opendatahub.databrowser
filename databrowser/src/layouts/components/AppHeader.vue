a6f6f3f3d3f049e9cf306c507503805f7124a532
Martin Malfertheiner <martin.malfertheiner@aboutbits.it>
2021-11-29 12:22:40 +0100

    header container styles

--- databrowser/src/layouts/components/AppHeader.vue
+++ databrowser/src/layouts/components/AppHeader.vue
@@ -1,24 +1,24 @@
 <template>
-  <header>
+  <header
+    class="flex flex-row justify-between mx-3 md:mx-auto w-full max-w-5xl h-20"
+  >
     <router-link to="/">
       <img
         src="/logo-open-data-hub-black.png"
         :alt="$t('header.logo')"
-        class="logo"
+        class="mt-5 logo"
       />
     </router-link>
-    <LanguageSwitcher></LanguageSwitcher>
     <UserAuthentication />
   </header>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@vue/runtime-core';
-import LanguageSwitcher from '../../components/language-switcher/LanguageSwitcher.vue';
 import UserAuthentication from '../../domain/auth/UserAuthentication.vue';
 
 export default defineComponent({
-  components: { UserAuthentication, LanguageSwitcher },
+  components: { UserAuthentication },
 });
 </script>
 
