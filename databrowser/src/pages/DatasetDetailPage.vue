697b95d30aac113561e82de52ac37c211b87d55e
Simon Planinschek <planinscheksimon@outlook.com>
2021-12-02 08:43:52 +0100

    Add Tabe

--- databrowser/src/pages/DatasetDetailPage.vue
+++ databrowser/src/pages/DatasetDetailPage.vue
@@ -1,6 +1,37 @@
 <template>
-  <div>
-    Dataset Detail Page (type = {{ $route.params.datasetType }}, ID =
-    {{ $route.params.datasetId }})
-  </div>
+  <ContentArea>
+    <TabGroup>
+      <TabList class="py-7 space-x-4 border-t border-b border-gray-300">
+        <Tab>Table view</Tab>
+        <Tab>Detail view</Tab>
+        <Tab>Raw Data</Tab>
+      </TabList>
+      <TabPanels>
+        <TabPanel>
+          <div>
+            Dataset Detail Page (type = {{ $route.params.datasetType }}, ID =
+            {{ $route.params.datasetId }})
+          </div>
+        </TabPanel>
+        <TabPanel>Details</TabPanel>
+        <TabPanel>Raw</TabPanel>
+      </TabPanels>
+    </TabGroup>
+  </ContentArea>
 </template>
+<script>
+import ContentArea from '../components/content/ContentArea.vue';
+import Tab from '../components/tabs/Tab.vue';
+import { TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/vue';
+
+export default {
+  components: {
+    ContentArea,
+    TabGroup,
+    TabList,
+    Tab,
+    TabPanel,
+    TabPanels,
+  },
+};
+</script>
