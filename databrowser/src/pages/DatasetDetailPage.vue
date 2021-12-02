24cb53d09e06f993eafd6ed2f470566d4d9ab54a
Simon Planinschek <planinscheksimon@outlook.com>
2021-12-02 17:26:44 +0100

    Add language picker

--- databrowser/src/pages/DatasetDetailPage.vue
+++ databrowser/src/pages/DatasetDetailPage.vue
@@ -1,10 +1,15 @@
 <template>
   <ContentArea>
     <TabGroup>
-      <TabList class="py-7 space-x-4 border-t border-b border-gray-300">
+      <TabList
+        class="flex relative py-7 space-x-4 border-t border-b border-gray-300"
+      >
         <Tab>Table view</Tab>
         <Tab>Detail view</Tab>
         <Tab>Raw Data</Tab>
+        <div class="absolute right-0">
+          <LanguagePicker />
+        </div>
       </TabList>
       <TabPanels>
         <TabPanel>
@@ -23,9 +28,11 @@
 import ContentArea from '../components/content/ContentArea.vue';
 import Tab from '../components/tabs/Tab.vue';
 import { TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/vue';
+import LanguagePicker from '../components/languageSwitcher/LanguageFilter.vue';
 
 export default {
   components: {
+    LanguagePicker,
     ContentArea,
     TabGroup,
     TabList,
