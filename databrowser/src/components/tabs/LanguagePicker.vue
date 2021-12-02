78e3f700d8d1583a82e00d6b4284edb4472adc88
Simon Planinschek <planinscheksimon@outlook.com>
2021-12-02 08:45:02 +0100

    Add components

---
+++ databrowser/src/components/tabs/LanguagePicker.vue
@@ -0,0 +1,20 @@
+<template>
+  <!--    <span class="relative z-0 inline-flex shadow-sm rounded-full">
+      <button type="button" class="relative inline-flex items-center px-4 py-2 rounded-l-full border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50">DE</button>
+      <button type="button" class="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50">IT</button>
+      <button type="button" class="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50">IT</button>
+      <button type="button" class="-ml-px relative inline-flex items-center px-4 py-2 border border-green-500 bg-green-100 z-10 text-sm font-medium text-green-700 hover:bg-gray-50">IT</button>
+      <button type="button" class="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50">IT</button>
+      <button type="button" class="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50">IT</button>
+      <button type="button" class="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50">IT</button>
+      <button type="button" class="-ml-px relative inline-flex items-center px-4 py-2 rounded-r-full border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50">EN</button>
+    </span>-->
+</template>
+
+<script>
+import { defineComponent } from '@vue/runtime-core';
+
+export default defineComponent({});
+</script>
+
+<style scoped></style>
---
+++ databrowser/src/components/tabs/Tab.vue
@@ -0,0 +1,31 @@
+<template>
+  <Tab v-slot="{ selected }">
+    <button
+      :class="[
+        selected
+          ? 'text-green-500 bg-opacity-10 bg-green-500'
+          : 'hover:bg-gray-200',
+      ]"
+      class="
+        group
+        py-1
+        px-5
+        font-semibold
+        rounded-full
+        text-center text-gray-700
+        border border-gray-400
+      "
+    >
+      <slot />
+    </button>
+  </Tab>
+</template>
+
+<script>
+import { defineComponent } from '@vue/runtime-core';
+import { Tab } from '@headlessui/vue';
+
+export default defineComponent({
+  components: { Tab },
+});
+</script>
