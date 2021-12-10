25b39560db8463b044e293f1d8403aa1e9b142c4
Simon Planinschek <planinscheksimon@outlook.com>
2021-12-10 08:29:08 +0100

    Add LanguagePicker component to reduce complexity

--- databrowser/src/pages/DatasetDetailPage.vue
+++ databrowser/src/pages/DatasetDetailPage.vue
@@ -8,20 +8,7 @@
         <Tab>Detail view</Tab>
         <Tab>Raw Data</Tab>
         <div class="absolute right-0">
-          <ButtonPill
-            class="inline-flex md:hidden items-center"
-            @click="showMobileSelect = true"
-          >
-            <span class="pr-2">{{ currentSelected }}</span>
-            <ArrowDown />
-          </ButtonPill>
-
-          <ButtonPillGroup
-            :data="suportedLanguages"
-            :initial-selected="currentSelected"
-            class="hidden md:inline-flex"
-            @selected-change="changeLanguage"
-          />
+          <LanguagePicker />
         </div>
       </TabList>
       <TabPanels>
@@ -36,93 +23,23 @@
       </TabPanels>
     </TabGroup>
   </ContentArea>
-  <Dialog
-    :open="showMobileSelect"
-    class="overflow-y-auto fixed inset-0 z-10"
-    @close="closeDialog"
-  >
-    <div class="flex items-end w-full min-h-screen">
-      <DialogOverlay class="fixed inset-0 bg-black opacity-30" />
-      <div class="relative py-3 w-full bg-white rounded-t">
-        <div class="flex flex-col justify-center px-3 space-y-3 w-full">
-          <button class="mx-auto" @click="closeDialog">
-            <IconClose />
-          </button>
-          <ButtonPill
-            v-for="language in suportedLanguages"
-            :key="language"
-            :class="[
-              isSelected(language)
-                ? 'text-green-500 bg-opacity-10 bg-green-500 border-green-500'
-                : 'border-gray-500',
-            ]"
-            @click="changeLanguage(language)"
-            >{{ language }}
-          </ButtonPill>
-        </div>
-      </div>
-    </div>
-  </Dialog>
 </template>
 
 <script class="ts">
 import ContentArea from '../components/content/ContentArea.vue';
 import Tab from '../components/tabs/Tab.vue';
-import {
-  Dialog,
-  DialogOverlay,
-  TabGroup,
-  TabList,
-  TabPanel,
-  TabPanels,
-} from '@headlessui/vue';
-import ButtonPillGroup from '../components/button/ButtonPillGroup.vue';
-import ButtonPill from '../components/button/ButtonPill.vue';
-import { FilterLanguage } from '../domain/api/configFilter.ts';
-import ArrowDown from '../components/svg/ArrowDown.vue';
-import IconClose from '../components/svg/IconClose.vue';
+import { TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/vue';
+import LanguagePicker from '../components/languageSwitcher/LanguagePicker.vue';
 
 export default {
   components: {
-    ButtonPillGroup,
+    LanguagePicker,
     ContentArea,
     TabGroup,
     TabList,
     Tab,
     TabPanel,
     TabPanels,
-    Dialog,
-    DialogOverlay,
-    ButtonPill,
-    IconClose,
-    ArrowDown,
-  },
-  data() {
-    return {
-      suportedLanguages: Object.values(FilterLanguage),
-      showMobileSelect: false,
-    };
-  },
-  computed: {
-    currentSelected() {
-      const query = this.$route.query.language;
-
-      return query != null && this.suportedLanguages.includes(query)
-        ? query
-        : FilterLanguage.EN;
-    },
-  },
-  methods: {
-    changeLanguage(language) {
-      this.$router.replace({ query: { language } });
-      this.closeDialog();
-    },
-    isSelected(current) {
-      return this.currentSelected == current;
-    },
-    closeDialog() {
-      this.showMobileSelect = false;
-    },
   },
 };
 </script>
