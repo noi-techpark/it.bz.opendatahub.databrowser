aaaef9686774947cdd844cedcfa2109f0075a99a
Simon Planinschek <planinscheksimon@outlook.com>
2021-12-10 08:16:24 +0100

    Add icon

--- databrowser/src/pages/DatasetDetailPage.vue
+++ databrowser/src/pages/DatasetDetailPage.vue
@@ -3,4 +3,14 @@
     Dataset Detail Page (type = {{ $route.params.datasetType }}, ID =
     {{ $route.params.datasetId }})
   </div>
+
+  <DownloadSection />
 </template>
+
+<script>
+import DownloadSection from '../components/download/DownloadSection.vue';
+
+export default {
+  components: { DownloadSection },
+};
+</script>
