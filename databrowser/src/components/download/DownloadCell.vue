6dec90117be8bdb9a0fbea3be27ee189461371ca
Simon Planinschek <planinscheksimon@outlook.com>
2021-12-10 08:15:18 +0100

    Add download section

---
+++ databrowser/src/components/download/DownloadCell.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    class="
+      inline-flex
+      items-center
+      py-3
+      px-5
+      space-x-10
+      max-w-xl
+      rounded-xl
+      border border-gray-300
+    "
+  >
+    <div class="inline-flex flex-col break-all">
+      <slot></slot>
+    </div>
+    <button class="p-4 rounded-xl border border-gray-300">Icon</button>
+  </div>
+</template>
---
+++ databrowser/src/components/download/DownloadSection.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="mx-auto max-w-5xl">
+    <div class="hidden lg:flex items-stretch space-x-4 h-full">
+      <span class="self-center">Retrieve data based on table settings</span>
+      <DownloadCell>
+        <span class="font-semibold">API DATASET</span>
+        <span class="font-mono text-sm"
+          >https://api.datahub.curlpi.datahub.com/v1/charges/ch_/istchat/juati/ch_3Juaty2eZvKYlo2C0
+          atahub</span
+        >
+      </DownloadCell>
+      <DownloadCell>
+        <span class="font-semibold whitespace-nowrap">CSV</span>
+      </DownloadCell>
+      <DownloadCell>
+        <span class="font-semibold whitespace-nowrap">JSON</span>
+      </DownloadCell>
+    </div>
+    <div class="inline-flex lg:hidden justify-end px-10 w-full">
+      <button class="p-3 rounded-full border">
+        <ArrowUp />
+      </button>
+    </div>
+  </div>
+</template>
+<script>
+import DownloadCell from './DownloadCell.vue';
+import ArrowUp from '../svg/ArrowUp.vue';
+
+export default {
+  components: { ArrowUp, DownloadCell },
+};
+</script>
