2acfd7e33073daf1c7a267f42646936587c80ed3
Simon Planinschek <planinscheksimon@outlook.com>
2021-12-10 11:29:21 +0100

    Add icons

---
+++ databrowser/src/components/svg/IconCopy.vue
@@ -0,0 +1,13 @@
+<template>
+  <svg
+    class="w-6 h-6"
+    fill="currentColor"
+    viewBox="0 0 20 20"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M9 2a2 2 0 00-2 2v8a2 2 0 002 2h6a2 2 0 002-2V6.414A2 2 0 0016.414 5L14 2.586A2 2 0 0012.586 2H9z"
+    />
+    <path d="M3 8a2 2 0 012-2v10h8a2 2 0 01-2 2H5a2 2 0 01-2-2V8z" />
+  </svg>
+</template>
