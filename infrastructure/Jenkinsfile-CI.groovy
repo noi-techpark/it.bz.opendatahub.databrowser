c46a1bc4bf1ef27a7042d5292702ca6c8f1c9abe
Alex Lanz <alex.lanz@aboutbits.it>
2021-12-10 13:47:42 +0100

    Enabling linting

--- infrastructure/Jenkinsfile-CI.groovy
+++ infrastructure/Jenkinsfile-CI.groovy
@@ -14,7 +14,7 @@ pipeline {
         }
         stage('Test') {
             steps {
-                //sh 'cd databrowser && npm run lint'
+                sh 'cd databrowser && npm run lint'
                 sh 'cd databrowser && npm run test'
             }
         }
