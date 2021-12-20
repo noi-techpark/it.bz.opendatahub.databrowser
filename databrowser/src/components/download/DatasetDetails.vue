bbbd2eec5a3c06fcf64991e75c38bb0a4a8c45eb
Simon Planinschek <planinscheksimon@outlook.com>
2021-12-20 15:10:24 +0100

    Confirm copy

--- databrowser/src/components/download/DatasetDetails.vue
+++ databrowser/src/components/download/DatasetDetails.vue
@@ -5,21 +5,26 @@
       <DownloadDescription>{{ datasetUrl }}</DownloadDescription>
     </div>
     <DownloadButton @clicked="copyToClipboard">
-      <IconCopy />
+      <IconCheck v-if="showOk" />
+      <IconCopy v-else />
       <span class="sr-only">Copy dataset url to the clipboard</span>
     </DownloadButton>
   </DownloadCell>
 </template>
-<script>
+
+<script lang="ts">
 import { defineComponent } from '@vue/runtime-core';
 import DownloadCell from './DownloadCell.vue';
 import DownloadTitle from './DownloadTitle.vue';
 import DownloadDescription from './DownloadDescription.vue';
 import DownloadButton from './DownloadButton.vue';
 import IconCopy from '../svg/IconCopy.vue';
+import { ref } from 'vue';
+import IconCheck from '../svg/IconCheck.vue';
 
 export default defineComponent({
   components: {
+    IconCheck,
     IconCopy,
     DownloadButton,
     DownloadDescription,
@@ -33,13 +38,22 @@ export default defineComponent({
     },
   },
   setup(props) {
+    const showOk = ref<boolean>(false);
+
+    function confirmCopy() {
+      showOk.value = true;
+
+      setTimeout(() => (showOk.value = false), 1000);
+    }
+
     function copyToClipboard() {
       if (navigator.clipboard) {
         navigator.clipboard.writeText(props.datasetUrl);
+        confirmCopy();
       }
     }
 
-    return { copyToClipboard };
+    return { copyToClipboard, showOk };
   },
 });
 </script>
