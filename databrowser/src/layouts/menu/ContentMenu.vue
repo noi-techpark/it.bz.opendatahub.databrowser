689378a489db3b5be270f1bc344d88754c7cf20a
Simon Planinschek <planinscheksimon@outlook.com>
2022-01-12 17:05:22 +0100

    Renamed files

---
+++ databrowser/src/layouts/menu/ContentMenu.vue
@@ -0,0 +1,136 @@
+<template>
+  <button v-if="dialogOpen" class="md:hidden" @click="closeDialog">
+    <IconClose />
+  </button>
+  <button v-else class="inline-flex space-x-4" @click="dialogOpen = true">
+    <IconMenu />
+    <span class="hidden md:block">{{ $t('header.menu.title') }}</span>
+  </button>
+  <Dialog
+    :open="dialogOpen"
+    class="fixed top-0 z-20 p-0 w-screen"
+    @close="closeDialog"
+  >
+    <DialogOverlay class="hidden md:block fixed inset-0 bg-black opacity-30" />
+    <div
+      class="
+        hidden
+        md:block
+        overflow-x-auto
+        absolute
+        top-0
+        left-0
+        px-4
+        h-screen
+        bg-white
+      "
+    >
+      <button class="block py-4" @click="closeDialog">
+        <IconClose />
+      </button>
+      <div class="inline-flex flex-row h-full divide-x-2">
+        <DatasetMenuSubList
+          :items="content"
+          :title="$t('header.menu.allDatasets')"
+          @select-category="changeFirstSubMenu"
+        />
+        <DatasetMenuSubList
+          v-if="fistSubMenu"
+          :items="fistSubMenu.categories ?? []"
+          :title="fistSubMenu.label"
+          @select-category="changeSecoundSubMenu"
+        />
+        <DatasetMenuSubList
+          v-if="secoundSubMenu"
+          :items="secoundSubMenu.categories ?? []"
+          :title="secoundSubMenu.label"
+        />
+      </div>
+    </div>
+    <div
+      class="
+        md:hidden
+        absolute
+        top-12
+        right-0
+        left-0
+        px-4
+        h-screen
+        bg-white
+        border-t
+      "
+    >
+      <DatasetMenuSubList
+        v-if="secoundSubMenu"
+        :items="secoundSubMenu.categories ?? []"
+        :title="secoundSubMenu.label"
+        show-arrow
+        @arrow-back="secoundSubMenu = undefined"
+        @close-dialog="closeDialog"
+      />
+      <DatasetMenuSubList
+        v-else-if="fistSubMenu"
+        :items="fistSubMenu.categories ?? []"
+        :title="fistSubMenu.label"
+        show-arrow
+        @select-category="changeSecoundSubMenu"
+        @arrow-back="fistSubMenu = undefined"
+        @close-dialog="closeDialog"
+      />
+      <DatasetMenuSubList
+        v-else
+        :items="content"
+        :title="$t('header.menu.allDatasets')"
+        @select-category="changeFirstSubMenu"
+        @close-dialog="closeDialog"
+      />
+    </div>
+  </Dialog>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from '@vue/runtime-core';
+import { ref } from 'vue';
+import DatasetMenuSubList, { MenuItem } from './DatasetMenuSubList.vue';
+import IconClose from '../../components/svg/IconClose.vue';
+import IconMenu from '../../components/svg/IconMenu.vue';
+
+export default defineComponent({
+  components: { IconMenu, IconClose, DatasetMenuSubList },
+  props: {
+    content: {
+      type: Array as PropType<Array<MenuItem>>,
+      required: true,
+    },
+  },
+  setup() {
+    const fistSubMenu = ref<MenuItem>();
+    const secoundSubMenu = ref<MenuItem>();
+    const dialogOpen = ref<boolean>(false);
+
+    function changeFirstSubMenu(menu: MenuItem) {
+      secoundSubMenu.value = undefined;
+      fistSubMenu.value = menu;
+    }
+
+    function changeSecoundSubMenu(menu: MenuItem) {
+      secoundSubMenu.value = menu;
+    }
+
+    function closeDialog() {
+      dialogOpen.value = false;
+      fistSubMenu.value = undefined;
+      secoundSubMenu.value = undefined;
+    }
+
+    return {
+      dialogOpen,
+      fistSubMenu,
+      secoundSubMenu,
+      closeDialog,
+      changeFirstSubMenu,
+      changeSecoundSubMenu,
+    };
+  },
+});
+</script>
--- databrowser/src/layouts/menu/ContentMenuSubList.vue
+++ databrowser/src/layouts/menu/ContentMenuSubList.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="flex flex-col w-full">
-    <span class="py-2 pl-2 text-xl font-semibold">{{ title }}</span>
+    <button class="inline-flex items-center" @click="$emit('arrow-back')">
+      <ArrowLeft v-if="showArrow" class="md:hidden" />
+      <span class="py-2 pl-2 text-xl font-semibold">{{ title }}</span>
+    </button>
     <ul>
       <li
         v-for="item in items"
@@ -34,6 +37,7 @@
           v-if="item.url"
           :to="item.url"
           class="flex-1 py-2 pr-24 pl-2 w-full text-left"
+          @click="$emit('close-dialog')"
           >{{ item.label }}
         </router-link>
       </li>
@@ -45,6 +49,7 @@
 import { defineComponent, PropType } from '@vue/runtime-core';
 import ArrowRight from '../../components/svg/ArrowRight.vue';
 import { ref } from 'vue';
+import ArrowLeft from '../../components/svg/ArrowLeft.vue';
 
 export type MenuItem = {
   label: string;
@@ -53,7 +58,7 @@ export type MenuItem = {
 };
 
 export default defineComponent({
-  components: { ArrowRight },
+  components: { ArrowLeft, ArrowRight },
   props: {
     items: {
       type: Object as PropType<Array<MenuItem>>,
@@ -63,8 +68,12 @@ export default defineComponent({
       type: String,
       required: true,
     },
+    showArrow: {
+      type: Boolean,
+      default: false,
+    },
   },
-  emits: ['selectCategory'],
+  emits: ['selectCategory', 'arrow-back', 'close-dialog'],
   setup(props, context) {
     const selected = ref<string>('');
 
