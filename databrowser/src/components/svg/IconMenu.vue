dfec736ed86676728418104c3c8e223a3b48bff4
Simon Planinschek <planinscheksimon@outlook.com>
2022-01-12 10:57:24 +0100

    Add IconMenu

---
+++ databrowser/src/components/svg/IconMenu.vue
@@ -0,0 +1,23 @@
+<template>
+  <svg
+    height="20px"
+    viewBox="0 0 20 20"
+    width="20px"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M2,19 L22,19 M2,5 L22,5 M2,12 L22,12"
+      fill="none"
+      stroke="#000"
+      stroke-width="2"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'IconMenu',
+};
+</script>
+
+<style scoped></style>
