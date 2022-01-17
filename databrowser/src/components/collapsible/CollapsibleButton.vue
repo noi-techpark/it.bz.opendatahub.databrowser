39b4329d7f1be1f07f92747134b1d0d6df4bd172
Simon Planinschek <planinscheksimon@outlook.com>
2022-01-17 12:30:12 +0100

    Add CollapsibleButton component

---
+++ databrowser/src/components/collapsible/CollapsibleButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <DisclosureButton v-slot="{ open }">
+    <div
+      class="
+        flex
+        items-center
+        space-x-2
+        font-semibold
+        text-green-500
+        hover:underline
+      "
+    >
+      <ArrowUp v-if="open" class="fill-current" />
+      <ArrowDown v-else class="fill-current" />
+      <span class="block">
+        <slot />
+      </span>
+    </div>
+  </DisclosureButton>
+</template>
+
+<script lang="ts" setup>
+import { DisclosureButton } from '@headlessui/vue';
+import ArrowUp from '../svg/ArrowUp.vue';
+import ArrowDown from '../svg/ArrowDown.vue';
+</script>
