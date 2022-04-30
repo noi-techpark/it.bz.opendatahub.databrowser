b5deeda704751b43ee97edfa47290654c2954c4b
gappc <gapp.christian@gmail.com>
2022-04-30 15:35:54 +0200

    Implement custom menu

---
+++ databrowser/src/components/menu/MenuCustomButton.vue
@@ -0,0 +1,7 @@
+<template>
+  <MenuButton><slot></slot></MenuButton>
+</template>
+
+<script setup lang="ts">
+import { MenuButton } from '@headlessui/vue';
+</script>
---
+++ databrowser/src/components/menu/MenuCustomItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <MenuItem v-slot="{ active }">
+    <div
+      class="menu-custom-item"
+      :class="[
+        active ? 'bg-gray-50' : '',
+        'group flex w-full items-center rounded px-4 py-2',
+      ]"
+    >
+      <slot class="bg-red-200"></slot>
+    </div>
+  </MenuItem>
+</template>
+
+<script setup lang="ts">
+import { MenuItem } from '@headlessui/vue';
+</script>
+
+<style scoped>
+.menu-custom-item > * {
+  @apply: text-left w-full;
+}
+</style>
