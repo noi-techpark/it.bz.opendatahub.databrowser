8ad43bf52072435d532efd4b849f7c5f4d9241eb
gappc <gapp.christian@gmail.com>
2022-05-01 13:23:08 +0200

    Implement radiobutton

---
+++ databrowser/src/components/radio/RadioCustom.vue
@@ -0,0 +1,20 @@
+<template>
+  <label class="cursor-pointer"
+    ><input
+      type="radio"
+      class="inline-block mr-4 text-green-500 checked:bg-none focus:ring-green-500"
+      :value="value"
+      :checked="value === modelValue"
+    />{{ label }}</label
+  >
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+defineProps<{
+  value: string;
+  label?: string;
+  modelValue?: string;
+}>();
+</script>
