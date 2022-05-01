645f7c4d464f77b9ad4a4375f3fd30d5ca2efd66
gappc <gapp.christian@gmail.com>
2022-05-01 13:19:05 +0200

    Add icons

---
+++ databrowser/src/components/svg/IconInfo.vue
@@ -0,0 +1,28 @@
+<template>
+  <svg
+    width="16"
+    height="17"
+    viewBox="0 0 16 17"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M7.99998 15.9286C12.1027 15.9286 15.4286 12.6027 15.4286 8.50001C15.4286 4.39733 12.1027 1.07144 7.99998 1.07144C3.8973 1.07144 0.571411 4.39733 0.571411 8.50001C0.571411 12.6027 3.8973 15.9286 7.99998 15.9286Z"
+      stroke-width="1.14286"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M8 8.50002V12.5"
+      stroke-width="1.14286"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M8.00002 6.2143C8.31561 6.2143 8.57145 5.95846 8.57145 5.64287C8.57145 5.32728 8.31561 5.07144 8.00002 5.07144C7.68443 5.07144 7.42859 5.32728 7.42859 5.64287C7.42859 5.95846 7.68443 6.2143 8.00002 6.2143Z"
+      stroke-width="1.14286"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
