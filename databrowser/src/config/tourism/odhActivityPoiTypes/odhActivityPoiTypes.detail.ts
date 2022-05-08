21f92d9d882e91290caa8441aea755bfc11acf03
gappc <gapp.christian@gmail.com>
2022-05-08 21:57:31 +0200

    Improve embedded config for ODH Activity POI types

--- databrowser/src/config/tourism/odhActivityPoiTypes/odhActivityPoiTypes.detail.ts
+++ databrowser/src/config/tourism/odhActivityPoiTypes/odhActivityPoiTypes.detail.ts
@@ -1,76 +1,74 @@
-import { CellComponent } from '../../../domain/cellComponents/types';
 import { ViewConfig } from '../../../domain/viewConfig/types';
 import { domains } from '../../../domain/openApi';
+import { odhActivityTypesPoiDescription } from './odhActivityPoiTypes.description';
+import { CellComponent } from '../../../domain/cellComponents/types';
 
 export const odhActivityPoiTypesDetail: ViewConfig = {
   source: 'embedded',
+  description: odhActivityTypesPoiDescription,
   baseUrl: domains.tourism.baseUrl,
   path: '/v1/ODHActivityPoiTypes/{id}',
   renderConfig: {
-    type: 'list',
+    type: 'detail',
     elements: [
       {
-        title: 'Image',
-        component: CellComponent.ImageCell,
-        class: 'w-40',
-        fields: {
-          src: 'ImageGallery.[0].ImageUrl',
-        },
-      },
-      {
-        title: 'Title',
-        component: CellComponent.StringCell,
-        class: 'w-48',
-        fields: {
-          text: 'Detail.{language}.Title',
-        },
-      },
-      {
-        title: 'Location',
-        component: CellComponent.TextHighlightCell,
-        class: 'w-40',
-        fields: {
-          title: 'LocationInfo.RegionInfo.Name.{language}',
-          subtitle: 'LocationInfo.MunicipalityInfo.Name.{language}',
-        },
-      },
-      {
-        title: 'Languages',
-        component: CellComponent.ArrayCell,
-        class: 'w-40',
-        fields: {
-          items: 'HasLanguage',
-        },
-        params: {
-          separator: ', ',
-        },
-      },
-      {
-        title: 'Edited',
-        component: CellComponent.EditedDateCell,
-        class: 'w-40',
-        fields: {
-          date: 'LastChange',
-        },
-        params: {
-          format: 'dd. MMMM yyyy',
-        },
-      },
-      {
-        title: 'Source',
-        component: CellComponent.StringCell,
-        class: 'w-36',
-        fields: {
-          text: 'Source',
-        },
-      },
-      {
-        title: 'ODH state',
-        component: CellComponent.StateCell,
-        class: 'w-36',
-        fields: {
-          state: 'OdhActive',
-        },
+        name: 'Main data',
+        slug: 'main-data',
+        subcategories: [
+          {
+            name: '',
+            properties: [
+              {
+                title: 'Id',
+                component: CellComponent.StringCell,
+                class: 'w-40',
+                fields: {
+                  text: 'Id',
+                },
+              },
+              {
+                title: 'Key',
+                component: CellComponent.StringCell,
+                class: 'w-40',
+                fields: {
+                  text: 'Key',
+                },
+              },
+              {
+                title: 'Type',
+                component: CellComponent.StringCell,
+                class: 'w-40',
+                fields: {
+                  text: 'Type',
+                },
+              },
+              {
+                title: 'TypeDesc',
+                component: CellComponent.StringCell,
+                class: 'w-40',
+                fields: {
+                  text: 'TypeDesc.{language}',
+                },
+              },
+              {
+                title: 'Bitmask',
+                component: CellComponent.StringCell,
+                class: 'w-40',
+                fields: {
+                  text: 'Bitmask',
+                },
+              },
+              {
+                title: 'Parent',
+                component: CellComponent.StringCell,
+                class: 'w-40',
+                fields: {
+                  text: 'Parent',
+                },
+              },
+            ],
+          },
+        ],
       },
     ],
   },
--- databrowser/src/config/tourism/odhActivityPoiTypes/odhActivityPoiTypes.list.ts
+++ databrowser/src/config/tourism/odhActivityPoiTypes/odhActivityPoiTypes.list.ts
@@ -1,18 +1,47 @@
 import { ViewConfig } from '../../../domain/viewConfig/types';
 import { domains } from '../../../domain/openApi';
+import { odhActivityTypesPoiDescription } from './odhActivityPoiTypes.description';
+import { CellComponent } from '../../../domain/cellComponents/types';
 
 export const odhActivityPoiTypesList: ViewConfig = {
   source: 'embedded',
+  description: odhActivityTypesPoiDescription,
   baseUrl: domains.tourism.baseUrl,
   path: '/v1/ODHActivityPoiTypes',
   renderConfig: {
-    type: 'detail',
+    type: 'list',
     elements: [
-      { name: 'Main data', slug: 'main-data', subcategories: [] },
       {
-        name: 'Text information',
-        slug: 'text-information',
-        subcategories: [],
+        title: 'Id',
+        component: CellComponent.StringCell,
+        class: 'w-40',
+        fields: {
+          text: 'Id',
+        },
+      },
+      {
+        title: 'Key',
+        component: CellComponent.StringCell,
+        class: 'w-40',
+        fields: {
+          text: 'Key',
+        },
+      },
+      {
+        title: 'Type',
+        component: CellComponent.StringCell,
+        class: 'w-40',
+        fields: {
+          text: 'Type',
+        },
+      },
+      {
+        title: 'TypeDesc',
+        component: CellComponent.StringCell,
+        class: 'w-40',
+        fields: {
+          text: 'TypeDesc.{language}',
+        },
       },
     ],
   },
