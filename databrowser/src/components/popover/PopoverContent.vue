0863c5b30ea17b91281d82af4c2c4c6e0e98c110
gappc <gapp.christian@gmail.com>
2022-08-05 14:07:00 +0200

    Add support for disabled attribute to PopoverContent

--- databrowser/src/components/popover/PopoverContent.vue
+++ databrowser/src/components/popover/PopoverContent.vue
@@ -1,14 +1,23 @@
 <template>
   <div
     class="p-4"
-    :class="{ 'hover:bg-gray-50 cursor-pointer': props.withHover }"
+    :class="{
+      'hover:bg-gray-50 cursor-pointer': props.withHover && !disabled,
+      'text-gray-300': disabled,
+    }"
   >
     <slot></slot>
   </div>
 </template>
 
 <script setup lang="ts">
-import { defineProps } from 'vue';
+import { defineProps, withDefaults } from 'vue';
 
-const props = defineProps<{ withHover?: boolean }>();
+const props = withDefaults(
+  defineProps<{ disabled?: boolean; withHover?: boolean }>(),
+  {
+    disabled: false,
+    withHover: false,
+  }
+);
 </script>
