ba9d0f35fd4e449bd2b9d6aa40fb2cdb2158bade
gappc <gapp.christian@gmail.com>
2022-08-06 15:12:50 +0200

    Implement component that wraps full-screen mode

---
+++ databrowser/src/components/fullscreen/UseFullscreen.vue
@@ -0,0 +1,21 @@
+<!-- This component is inspired by VueUse https://vueuse.org/core/usefullscreen/#component-usage -->
+
+<template>
+  <div ref="target">
+    <slot
+      :is-supported="data.isSupported"
+      :is-fullscreen="data.isFullscreen"
+      :enter="data.enter"
+      :exit="data.exit"
+      :toggle="data.toggle"
+    ></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { reactive, ref } from 'vue';
+import { useFullscreen } from '@vueuse/core';
+
+const target = ref();
+const data = reactive(useFullscreen(target));
+</script>
