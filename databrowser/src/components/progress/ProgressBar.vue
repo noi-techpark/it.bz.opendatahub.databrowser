c2e440cc502136dd6efef335ddde13207da33326
gappc <gapp.christian@gmail.com>
2022-08-07 14:06:46 +0200

    Implement progress bar

---
+++ databrowser/src/components/progress/ProgressBar.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="w-full bg-gray-300">
+    <div :style="{ width: `${percentage}%` }" class="h-1 bg-green-500"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+defineProps<{ percentage: number }>();
+</script>
