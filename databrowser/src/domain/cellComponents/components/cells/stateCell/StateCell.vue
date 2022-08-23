c9de4046d94dbcbb5089a968fca43cdccee9c76f
ila131 <ilrusso@posteo.net>
2022-08-23 14:41:49 +0200

    restyle active/inactive badges

--- databrowser/src/domain/cellComponents/components/cells/stateCell/StateCell.vue
+++ databrowser/src/domain/cellComponents/components/cells/stateCell/StateCell.vue
@@ -2,7 +2,7 @@
 <template>
   <div v-if="state != null">
     <span v-if="state === true" class="active">Active</span>
-    <span v-else class="not-active">Not active</span>
+    <span v-else class="not-active">Inactive</span>
   </div>
 </template>
 
@@ -21,15 +21,27 @@ withDefaults(
 
 <style>
 .active {
-  @apply py-1 px-5 rounded-full border;
+  @apply py-1 px-5  border;
   background-color: rgba(61, 149, 252, 0.1);
-  border-color: #3d95fc;
-  color: #3d95fc;
+  display: list-item;
+  list-style-position: inside;
+  margin-right: 1.2rem;
+  padding-left: 0.8rem;
+  padding-top: 0.1rem;
+  padding-bottom: 0.1rem;
+  border-radius: 6px;
+  color: #2264e5;
 }
 .not-active {
   @apply py-1 px-5 rounded-full border;
   background-color: rgba(255, 25, 87, 0.1);
-  border-color: #ff1957;
-  color: #ff1957;
+  display: list-item;
+  list-style-position: inside;
+  margin-right: 1rem;
+  padding-left: 0.8rem;
+  padding-top: 0.1rem;
+  padding-bottom: 0.1rem;
+  border-radius: 6px;
+  color: #d12953;
 }
 </style>
