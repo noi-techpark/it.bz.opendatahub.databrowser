5a1b77bff41b58426743d81ada4955be8fee020f
lukas <lukas.mayr01@gmail.com>
2022-08-24 11:20:04 +0200

    add detailView for wineAward

--- databrowser/src/config/tourism/wineAward/wineAward.views.ts
+++ databrowser/src/config/tourism/wineAward/wineAward.views.ts
@@ -1,5 +1,7 @@
 import { wineAwardListView } from './wineAward.listView';
+import { wineAwardDetailView } from './wineAward.detailView';
 
 export const wineAwardViews = {
   table: wineAwardListView,
+  detail: wineAwardDetailView,
 };
