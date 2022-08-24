e9b9be091a4f3e930111890ca0ad991f68731e2a
ila131 <ilrusso@posteo.net>
2022-08-24 17:21:09 +0200

    fix padding active/inactive badges

--- databrowser/src/domain/cellComponents/components/cells/stateCell/StateCell.vue
+++ databrowser/src/domain/cellComponents/components/cells/stateCell/StateCell.vue
@@ -21,27 +21,36 @@ withDefaults(
 
 <style>
 .active {
-  @apply py-1 px-5  border;
+  @apply py-1 px-5;
   background-color: rgba(61, 149, 252, 0.1);
   display: list-item;
   list-style-position: inside;
-  margin-right: 1.2rem;
-  padding-left: 0.8rem;
-  padding-top: 0.1rem;
-  padding-bottom: 0.1rem;
+  padding-left: 10px;
+  padding-right: 10px;
+  padding-top: 2px;
+  padding-bottom: 4px;
+  padding: none;
+  float: left;
+  clear: left;
   border-radius: 6px;
   color: #2264e5;
+  letter-spacing: 0.5px;
 }
+
 .not-active {
-  @apply py-1 px-5 rounded-full border;
+  @apply py-1 px-5;
   background-color: rgba(255, 25, 87, 0.1);
   display: list-item;
   list-style-position: inside;
-  margin-right: 1rem;
-  padding-left: 0.8rem;
-  padding-top: 0.1rem;
-  padding-bottom: 0.1rem;
+  padding-left: 10px;
+  padding-right: 10px;
+  padding-top: 2px;
+  padding-bottom: 4px;
+  padding: none;
+  float: left;
+  clear: left;
   border-radius: 6px;
   color: #d12953;
+  letter-spacing: 0.5px;
 }
 </style>
