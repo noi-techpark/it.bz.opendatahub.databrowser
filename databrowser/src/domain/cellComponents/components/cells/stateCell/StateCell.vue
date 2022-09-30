70263663a07fad85f46dd9b95b02587e6b69e92b
ila131 <ilrusso@posteo.net>
2022-09-30 09:00:31 +0200

    use tailwindcss

--- databrowser/src/domain/cellComponents/components/cells/stateCell/StateCell.vue
+++ databrowser/src/domain/cellComponents/components/cells/stateCell/StateCell.vue
@@ -19,37 +19,16 @@ withDefaults(
 );
 </script>
 
-<style>
+<style scoped>
+span {
+  @apply list-item list-inside py-1 px-2.5 rounded float-left;
+}
 .active {
-  @apply py-1 px-5;
   background-color: rgba(61, 149, 252, 0.1);
-  display: list-item;
-  list-style-position: inside;
-  padding-left: 10px;
-  padding-right: 10px;
-  padding-top: 2px;
-  padding-bottom: 4px;
-  padding: none;
-  float: left;
-  clear: left;
-  border-radius: 6px;
   color: #2264e5;
-  letter-spacing: 0.5px;
 }
 .not-active {
-  @apply py-1 px-5;
   background-color: rgba(255, 25, 87, 0.1);
-  display: list-item;
-  list-style-position: inside;
-  padding-left: 10px;
-  padding-right: 10px;
-  padding-top: 2px;
-  padding-bottom: 4px;
-  padding: none;
-  float: left;
-  clear: left;
-  border-radius: 6px;
   color: #d12953;
-  letter-spacing: 0.5px;
 }
 </style>
