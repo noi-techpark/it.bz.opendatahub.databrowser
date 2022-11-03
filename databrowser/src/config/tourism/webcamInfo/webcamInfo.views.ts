dcf2eea385a1bfafc275fb32e0e28bc1615baa64
Sergio Evola <Evola.sergio97@gmail.com>
2022-11-03 15:11:56 +0100

    first implementation of webcamInfo detail view

--- databrowser/src/config/tourism/webcamInfo/webcamInfo.views.ts
+++ databrowser/src/config/tourism/webcamInfo/webcamInfo.views.ts
@@ -1,7 +1,9 @@
 import { webcamInfoListView } from './webcamInfo.listView';
 import { webcamInfoDetailView } from './webcamInfo.detailView';
+import { webcamInfoEditView } from './webcamInfo.editView';
 
 export const webcamInfoViews = {
   table: webcamInfoListView,
   detail: webcamInfoDetailView,
+  edit: webcamInfoEditView
 };
