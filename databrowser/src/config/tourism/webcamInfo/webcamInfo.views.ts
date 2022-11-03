24ba2c393fd1b613e324cf09322f071cae24adc8
Sergio Evola <Evola.sergio97@gmail.com>
2022-11-03 15:36:19 +0100

    Revert "first implementation of webcamInfo detail view"
    This reverts commit dcf2eea385a1bfafc275fb32e0e28bc1615baa64.

--- databrowser/src/config/tourism/webcamInfo/webcamInfo.views.ts
+++ databrowser/src/config/tourism/webcamInfo/webcamInfo.views.ts
@@ -1,9 +1,7 @@
 import { webcamInfoListView } from './webcamInfo.listView';
 import { webcamInfoDetailView } from './webcamInfo.detailView';
-import { webcamInfoEditView } from './webcamInfo.editView';
 
 export const webcamInfoViews = {
   table: webcamInfoListView,
   detail: webcamInfoDetailView,
-  edit: webcamInfoEditView
 };
