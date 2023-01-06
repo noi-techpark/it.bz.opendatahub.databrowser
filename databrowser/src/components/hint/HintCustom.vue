d82d62c21fc1b84b78257fb16e1cd11b9485d284
gappc <gapp.christian@gmail.com>
2023-01-06 22:37:33 +0100

    Implement custom hint

---
+++ databrowser/src/components/hint/HintCustom.vue
@@ -0,0 +1,51 @@
+<template>
+  <div :class="[classNames.background, classNames.text]" class="px-4 py-2">
+    <div class="flex items-center justify-between gap-3">
+      <div class="grow font-semibold">{{ title }}</div>
+      <button @click="emit('close')">
+        <IconClose class="h-4 w-4 rounded-full border" />
+      </button>
+    </div>
+    <div class="mr-8"><slot></slot></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed, defineProps } from 'vue';
+import IconClose from '../svg/IconClose.vue';
+import { HintType } from './types';
+
+interface Color {
+  background: string;
+  text: string;
+}
+
+const types: Record<string, Color> = {
+  calm: {
+    background: 'bg-dialog',
+    text: 'text-white',
+  },
+  info: {
+    background: 'bg-hint-info-secondary',
+    text: 'text-hint-info',
+  },
+  warning: {
+    background: 'bg-hint-warning-secondary',
+    text: 'text-hint-warning',
+  },
+  error: {
+    background: 'bg-hint-error-secondary',
+    text: 'text-hint-error',
+  },
+};
+
+const emit = defineEmits(['close']);
+
+const props = defineProps<{
+  type: HintType;
+  title?: string;
+  content?: string;
+}>();
+
+const classNames = computed(() => types[props.type]);
+</script>
