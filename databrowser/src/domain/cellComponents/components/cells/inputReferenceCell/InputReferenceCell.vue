ea5561b549ca4fce86f9178cef28cecec785bd63
gappc <gapp.christian@gmail.com>
2023-01-09 22:09:13 +0100

    Improve input reference cell
    - add editable support
    - add support to allow empty value
    - add support to sort by label

--- databrowser/src/domain/cellComponents/components/cells/inputReferenceCell/InputReferenceCell.vue
+++ databrowser/src/domain/cellComponents/components/cells/inputReferenceCell/InputReferenceCell.vue
@@ -1,54 +1,87 @@
 <template>
-  <div>
+  <div v-if="isWriteable">
     <div v-if="isLoading" class="animate-pulse">Loading...</div>
     <div v-if="isError" class="text-red-500">
       <pre>{{ JSON.stringify(error, null, 2) }}</pre>
     </div>
     <div v-if="isSuccess">
       <SelectCustom :options="options" :size="SelectSize.md" @change="change" />
-      <div v-if="unknownValue" class="text-red-400">
+      <div v-if="showUnknownError" class="text-red-400">
         Attention: current value "{{ value }}" is unknown
       </div>
     </div>
   </div>
+  <span v-else>{{ selectedLabel }}</span>
 </template>
 
 <script setup lang="ts">
-import { computed, defineEmits, defineProps, toRefs } from 'vue';
+import { computed, defineEmits, defineProps, toRefs, withDefaults } from 'vue';
 import { useQuery } from 'vue-query';
 import { useAxiosFetcher } from '../../../../api';
 import SelectCustom from '../../../../../components/select/SelectCustom.vue';
 import { SelectSize } from '../../../../../components/select/types';
-import { buildMapper } from './mapper';
+import { useMapper } from './mapper';
+import { useWriteable } from '../../utils/writeable/useWriteable';
 
 const emit = defineEmits(['update']);
 
-const props = defineProps<{
-  value?: string | boolean | number;
-  url: string;
-  keySelector: string;
-  labelSelector: string;
-  required?: boolean;
-}>();
+const props = withDefaults(
+  defineProps<{
+    value?: string | boolean | number;
+    url: string;
+    keySelector: string;
+    labelSelector: string;
+    sortByLabel?: boolean;
+    emptyAllowed?: boolean;
+    editable?: boolean;
+    readonly?: string | boolean;
+  }>(),
+  {
+    value: undefined,
+    sortByLabel: true,
+    emptyAllowed: true,
+    editable: true,
+    readonly: false,
+  }
+);
 
-const { url, keySelector, labelSelector, value, required } = toRefs(props);
+const {
+  url,
+  keySelector,
+  labelSelector,
+  value,
+  sortByLabel,
+  emptyAllowed,
+  editable,
+  readonly,
+} = toRefs(props);
+
+const isWriteable = useWriteable({ editable, readonly });
 
 const queryKey = url;
 const queryFn = useAxiosFetcher();
-const select = buildMapper(keySelector, labelSelector, value, required);
+const response = useQuery({ queryKey, queryFn });
 
-const response = useQuery({ queryKey, queryFn, select });
+const { options, unknownValue } = useMapper(
+  response.data,
+  value,
+  keySelector,
+  labelSelector,
+  sortByLabel,
+  emptyAllowed
+);
 
 const isLoading = computed(() => response.isLoading.value);
 const isSuccess = computed(() => response.isSuccess.value);
 const isError = computed(() => response.isError.value);
 const error = computed(() => response.error.value);
-const options = computed(() => response.data.value || []);
 
-const unknownValue = computed(
-  () =>
-    value?.value != null &&
-    options.value.find((o) => o.selected === true) == null
+const selectedLabel = computed(
+  () => options.value.find((option) => option.selected)?.label
+);
+
+const showUnknownError = computed(
+  () => unknownValue.value && !(emptyAllowed.value && value.value === '')
 );
 
 const change = (value: string) => emit('update', { prop: 'value', value });
--- databrowser/src/domain/cellComponents/components/cells/inputReferenceCell/mapper.ts
+++ databrowser/src/domain/cellComponents/components/cells/inputReferenceCell/mapper.ts
@@ -1,35 +1,70 @@
 import { AxiosResponse } from 'axios';
-import { Ref } from 'vue';
+import { computed, ref, Ref, watch } from 'vue';
 import { SelectOption } from '../../../../../components/select/types';
 import * as R from 'ramda';
+import {
+  replacePlaceholders,
+  useApiParameterReplacements,
+} from '../../../../api';
 
-export const buildMapper =
-  (
-    keySelector: Ref<string>,
-    labelSelector: Ref<string>,
-    value?: Ref<string | boolean | number | undefined>,
-    required?: Ref<boolean | undefined>
-  ) =>
-  (axiosResponse: unknown): SelectOption[] => {
-    const data = (axiosResponse as AxiosResponse<unknown[]>).data;
+export const useMapper = (
+  data: Ref<undefined> | Ref<AxiosResponse<unknown[], any>>,
+  initialValue: Ref<string | boolean | number | unknown>,
+  keySelector: Ref<string>,
+  labelSelector: Ref<string>,
+  sortByLabel: Ref<boolean>,
+  emptyAllowed: Ref<boolean>
+) => {
+  const options = ref<SelectOption[]>([]);
 
-    const mappedData = data
-      .map((item) => {
-        const v = getPropertyValue(item, keySelector.value);
-        const l = getPropertyValue(item, labelSelector.value);
-        const selected = value?.value === v;
+  const replacements = useApiParameterReplacements();
+
+  watch(
+    () => [data.value, initialValue.value, replacements.value],
+    () => {
+      if (data.value == null) {
+        return;
+      }
+
+      // Compute key and value selectors with replacements (e.g. current language)
+      const replace = (s: string): string =>
+        replacePlaceholders(s, replacements.value);
+      const keySelectorWithReplacements = replace(keySelector.value);
+      const labelSelectorWithReplacements = replace(labelSelector.value);
+
+      options.value = data.value.data.map((item) => {
+        const v = getPropertyValue(item, keySelectorWithReplacements);
+        const l = getPropertyValue(item, labelSelectorWithReplacements);
+        const selected = initialValue?.value === v;
         return {
           value: v,
-          label: l,
+          label: l ?? v,
           selected,
         };
-      })
-      .sort((a, b) => a.label.localeCompare(b.label));
+      });
 
-    return required?.value === true
-      ? mappedData
-      : [{ label: '--- Please select ---', value: '' }, ...mappedData];
-  };
+      if (sortByLabel) {
+        options.value.sort((a, b) => a.label.localeCompare(b.label));
+      }
+
+      if (emptyAllowed.value) {
+        options.value = [
+          { label: '--- No value ---', value: '' },
+          ...options.value,
+        ];
+      }
+    },
+    { immediate: true }
+  );
+
+  const unknownValue = computed(
+    () =>
+      initialValue.value != null &&
+      options.value.find((o) => o.selected === true) == null
+  );
+
+  return { options, unknownValue };
+};
 
 const getPropertyValue = (item: unknown, jsonPath: string) => {
   const path = jsonPath.split('.');
