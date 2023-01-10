9f0e834333990d6b7bfb1de653a78eafb22308ed
gappc <gapp.christian@gmail.com>
2023-01-10 12:02:04 +0100

    Add hotjar for tracking and feedback

--- databrowser/src/env.d.ts
+++ databrowser/src/env.d.ts
@@ -16,6 +16,7 @@ interface ImportMetaEnv
   readonly VITE_APP_IMAGE_UPLOAD_URL: string;
   readonly VITE_APP_FILE_UPLOAD_URL: string;
   readonly VITE_APP_ODH_LOOKUP_BASE_URL: string;
+  readonly VITE_APP_HOTJAR_ID: string;
 }
 
 interface ImportMeta {
