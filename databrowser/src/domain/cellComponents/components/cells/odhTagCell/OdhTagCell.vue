b0c938691bc34d5eeed1daafa7ff44ae2c4b2bad
gappc <gapp.christian@gmail.com>
2023-01-19 19:11:31 +0100

    Simplify OdhTagCell implementation
    This commit also fixes input prop types.

--- databrowser/src/domain/cellComponents/components/cells/odhTagCell/OdhTagCell.vue
+++ databrowser/src/domain/cellComponents/components/cells/odhTagCell/OdhTagCell.vue
@@ -1,17 +1,11 @@
 <template>
-  <EditListCell :items="odhTagsAsRecord" :editable="editable">
+  <EditListCell :items="odhTags">
     <template #table="{ items }">
-      <OdhTagTable
-        v-if="url"
-        :items="items"
-        :url="url"
-        :editable="editable"
-        @update="update"
-      />
+      <OdhTagTable v-if="url" :items="items" :url="url" />
       <div v-else>
         <AlertError
           title="Can not display Open Data Hub Tags, no URL defined"
-          content="This seems to be a configuration problem. Please contact  support@opendatahub.com"
+          content="This seems to be a configuration problem. Please contact support@opendatahub.com"
         />
       </div>
     </template>
@@ -19,29 +13,13 @@
 </template>
 
 <script setup lang="ts">
-import { computed, defineProps } from 'vue';
+import { defineProps } from 'vue';
 import AlertError from '../../../../../components/alert/AlertError.vue';
 import EditListCell from '../../utils/editList/EditListCell.vue';
 import OdhTagTable from './OdhTagTable.vue';
 
-const emit = defineEmits(['update']);
-
-const props = defineProps<{
+defineProps<{
   odhTags?: string[] | null;
   url?: string;
-  editable?: boolean;
 }>();
-
-const odhTagsAsRecord = computed(() => {
-  if (props.odhTags == null) {
-    return [];
-  }
-
-  return props.odhTags.map((tag) => ({ tag }));
-});
-
-const update = (tags: { tag: string }[]) => {
-  const value = tags.reduce<string[]>((prev, curr) => [...prev, curr.tag], []);
-  emit('update', { prop: 'odhTags', value });
-};
 </script>
