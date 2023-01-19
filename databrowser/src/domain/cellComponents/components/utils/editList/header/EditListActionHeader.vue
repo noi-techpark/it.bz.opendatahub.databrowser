b34aaef95835b16dedd80efd135ea5127c36c896
gappc <gapp.christian@gmail.com>
2023-01-19 14:23:19 +0100

    Unify EditList action header
    This commit unifies the implementations of EditList table and tab
    components.

--- databrowser/src/domain/cellComponents/components/utils/editList/header/EditListActionHeader.vue
+++ databrowser/src/domain/cellComponents/components/utils/editList/header/EditListActionHeader.vue
@@ -1,21 +1,23 @@
 <template>
-  <div
-    class="mt-2 mb-5 flex justify-between gap-5 md:justify-end"
-    :class="anyItemSelected ? 'text-default' : 'text-disabled'"
-  >
+  <div>
+    <!--
+      The dialog can be hidden here using class "hidden"
+      because it uses a teleport to be rendered elsewhere 
+    -->
     <EditListDeleteDialog
+      class="hidden"
       title="Confirm deletion"
       description="Are you sure you want to delete all selected items?"
       :show-dialog="showDialog"
       @close="showDialog = false"
-      @confirm-delete="emitItemsSelected"
+      @confirm-delete="deleteSelectedItems"
     />
 
     <slot name="addItems"></slot>
 
     <EditListDeleteButton
       :disabled="!anyItemSelected"
-      text="Delete"
+      :text="deleteLabel"
       @click="showDialog = true"
     />
   </div>
@@ -26,14 +28,14 @@ import { defineEmits, defineProps, ref } from 'vue';
 import EditListDeleteButton from '../EditListDeleteButton.vue';
 import EditListDeleteDialog from '../dialogs/EditListDeleteDialog.vue';
 
-const emit = defineEmits(['deleteSelectedItems']);
+const emit = defineEmits(['confirmDelete']);
 
 const showDialog = ref(false);
 
-defineProps<{ anyItemSelected: boolean }>();
+defineProps<{ anyItemSelected: boolean; deleteLabel: string }>();
 
-const emitItemsSelected = () => {
+const deleteSelectedItems = () => {
   showDialog.value = false;
-  emit('deleteSelectedItems');
+  emit('confirmDelete');
 };
 </script>
