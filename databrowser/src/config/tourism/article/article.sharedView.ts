289b20eb32a1e051d4217caa759b3b613821812f
gappc <gapp.christian@gmail.com>
2023-01-24 20:57:57 +0100

    Add license section to Article

--- databrowser/src/config/tourism/article/article.sharedView.ts
+++ databrowser/src/config/tourism/article/article.sharedView.ts
@@ -111,5 +111,44 @@ export const articleSharedView = (): DetailViewConfig | EditViewConfig => ({
         },
       ],
     },
+    {
+      name: 'License',
+      slug: 'license',
+      subcategories: [
+        {
+          name: '',
+          properties: [
+            {
+              title: 'License',
+              component: CellComponent.SelectWithOptionsCell,
+              fields: { value: 'LicenseInfo.License' },
+              params: {
+                value_001: 'CC0',
+                label_001: 'CC0',
+                value_002: 'CC-BY',
+                label_002: 'CC-BY',
+                value_003: 'Closed',
+                label_003: 'Closed',
+              },
+            },
+            {
+              title: 'Author',
+              component: CellComponent.StringCell,
+              fields: { text: 'LicenseInfo.Author' },
+            },
+            {
+              title: 'License Holder',
+              component: CellComponent.UrlCell,
+              fields: { text: 'LicenseInfo.LicenseHolder' },
+            },
+            {
+              title: 'Closed Data',
+              component: CellComponent.ToggleCell,
+              fields: { enabled: 'LicenseInfo.ClosedData' },
+            },
+          ],
+        },
+      ],
+    },
   ],
 });
