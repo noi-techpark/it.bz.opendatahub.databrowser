0c484f4333b0eca678a343854ce34a01602b997d
gappc <gapp.christian@gmail.com>
2023-01-28 23:55:46 +0100

    Implement generic loading and error indicator

---
+++ databrowser/src/components/loading/LoadingError.vue
@@ -0,0 +1,13 @@
+<template>
+  <AlertError
+    title="Error while loading data"
+    :content="toErrorString(error)"
+  />
+</template>
+
+<script setup lang="ts">
+import AlertError from '../alert/AlertError.vue';
+import { toErrorString } from '../../domain/api/service/utils';
+
+defineProps<{ error: unknown }>();
+</script>
---
+++ databrowser/src/components/loading/LoadingState.vue
@@ -0,0 +1,17 @@
+<template>
+  <div v-if="isLoading || isError">
+    <LoadingIndicator v-if="isLoading" />
+    <LoadingError v-if="isError" :error="error" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import LoadingError from './LoadingError.vue';
+import LoadingIndicator from './LoadingIndicator.vue';
+
+defineProps<{
+  isLoading: boolean;
+  isError: boolean;
+  error?: unknown;
+}>();
+</script>
