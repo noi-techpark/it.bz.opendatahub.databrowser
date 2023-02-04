ba2f22d5090ab6ba0a51fafe2afed3dac0ea3eac
gappc <gapp.christian@gmail.com>
2023-02-04 23:34:09 +0100

    Add image placeholder

---
+++ databrowser/src/components/svg/IconImage.vue
@@ -0,0 +1,41 @@
+<template>
+  <svg
+    width="14"
+    height="14"
+    viewBox="0 0 14 14"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clip-path="url(#clip0_1431_9781)">
+      <path
+        d="M1.51001 13.5L12.51 13.5C13.0623 13.5 13.51 13.0523 13.51 12.5L13.51 1.5C13.51 0.947715 13.0623 0.5 12.51 0.5L1.51001 0.5C0.957725 0.5 0.51001 0.947715 0.51001 1.5L0.51001 12.5C0.51001 13.0523 0.957725 13.5 1.51001 13.5Z"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M9.25 6.5C10.2165 6.5 11 5.7165 11 4.75C11 3.7835 10.2165 3 9.25 3C8.2835 3 7.5 3.7835 7.5 4.75C7.5 5.7165 8.2835 6.5 9.25 6.5Z"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M9.4 13.5001C9.07801 11.785 8.15715 10.2402 6.80173 9.14125C5.44632 8.04226 3.74452 7.4606 2 7.50005C1.49668 7.49871 0.994381 7.54559 0.5 7.64005"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M13.5 9.91002C12.6946 9.63803 11.8501 9.49954 11 9.50002C9.92394 9.49768 8.85877 9.71548 7.87 10.14"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_1431_9781">
+        <rect width="14" height="14" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+</template>
