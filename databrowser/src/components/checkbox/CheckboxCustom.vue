bdff03b3ade5a4429ecd8bcabc353c5e61752adc
gappc <gapp.christian@gmail.com>
2023-04-28 22:39:29 +0200

    Add support to prevent checkbox being tabbable
    If "tabbable" is set to false, the checkbox can not be reached by
    keyboard tab anymore. This is helpful e.g. in case the checkbox is
    part of a button.

--- databrowser/src/components/checkbox/CheckboxCustom.vue
+++ databrowser/src/components/checkbox/CheckboxCustom.vue
@@ -5,6 +5,7 @@
         v-model="checked"
         type="checkbox"
         class="h-5 w-5 cursor-pointer rounded border-gray-400 text-green-700"
+        :tabindex="tabbable ? undefined : -1"
       />
       <span v-if="label != null" class="ml-3 font-semibold">{{ label }}</span>
     </label>
@@ -12,14 +13,21 @@
 </template>
 
 <script setup lang="ts">
-import { computed, defineEmits, defineProps } from 'vue';
+import { computed } from 'vue';
 
 const emit = defineEmits(['update:modelValue']);
 
-const props = defineProps<{
-  modelValue: boolean;
-  label?: string;
-}>();
+const props = withDefaults(
+  defineProps<{
+    modelValue: boolean;
+    label?: string;
+    tabbable?: boolean;
+  }>(),
+  {
+    label: undefined,
+    tabbable: true,
+  }
+);
 
 const checked = computed({
   get: () => props.modelValue,
