03416e30e48e5e4262bf636489f5d0ac6a7b73f2
henri <henri@moosbauer.com>
2023-07-28 08:41:31 +0200

    fix navbar breakpoint

--- databrowser/src/layouts/AppHeader.vue
+++ databrowser/src/layouts/AppHeader.vue
@@ -29,7 +29,7 @@ SPDX-License-Identifier: AGPL-3.0-or-later
     </div>
     <div class="bg-white">
       <ContentAlignmentX
-        class="m-auto flex flex-col gap-x-20 gap-y-2 px-4 py-1 md:flex-row xl:w-default 2xl:px-0"
+        class="m-auto flex flex-col gap-x-20 gap-y-2 px-4 py-1 lg:flex-row xl:w-default 2xl:px-0"
       >
         <div class="flex items-center">
           <router-link
