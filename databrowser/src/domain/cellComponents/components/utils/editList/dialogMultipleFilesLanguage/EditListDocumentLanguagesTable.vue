e0d9245a6161f6129a27430c5e96cf37d5ad8c0b
Alessio Crea <alessio.crea98@gmail.com>
2023-10-18 15:16:58 +0200

    fix: remove ref import EditListDocumentLanguagesTable

--- databrowser/src/domain/cellComponents/components/utils/editList/dialogMultipleFilesLanguage/EditListDocumentLanguagesTable.vue
+++ databrowser/src/domain/cellComponents/components/utils/editList/dialogMultipleFilesLanguage/EditListDocumentLanguagesTable.vue
@@ -59,7 +59,7 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue';
+import { computed } from 'vue';
 import TableHeaderCell from '../../../../../../components/table/TableHeaderCell.vue';
 import TableCell from '../../../../../../components/table/TableCell.vue';
 import CheckboxCustom from '../../../../../../components/checkbox/CheckboxCustom.vue';
