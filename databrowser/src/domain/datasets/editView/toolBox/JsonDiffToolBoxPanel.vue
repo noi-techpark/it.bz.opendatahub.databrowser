f096a40f309330597e039b5fec6691301777f742
gappc <gapp.christian@gmail.com>
2023-11-07 10:53:41 +0100

    Add fullscreen mode to JSON diff viewer

--- databrowser/src/domain/datasets/editView/toolBox/JsonDiffToolBoxPanel.vue
+++ databrowser/src/domain/datasets/editView/toolBox/JsonDiffToolBoxPanel.vue
@@ -5,26 +5,33 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 -->
 
 <template>
-  <ToolBoxPanel
-    class="overflow-auto border-transparent bg-green-500 pl-2"
-    :class="{ 'bg-red-500': !editStore.isEqual }"
+  <UseFullscreen
+    v-slot="{ toggle, isFullscreen }"
+    class="flex flex-col items-center justify-center md:items-stretch"
   >
-    <div class="h-[30rem] w-default">
-      <VueDiff
-        :mode="'split'"
-        :theme="'dark'"
-        :language="'json'"
-        :prev="editStore.initialAsJson ?? ''"
-        :current="editStore.currentAsJson ?? ''"
-        :input-delay="10"
-      />
-    </div>
-  </ToolBoxPanel>
+    <button @click="toggle">Toggle fullscreen</button>
+    <ToolBoxPanel
+      class="overflow-auto border-transparent bg-green-500 pl-2"
+      :class="{ 'bg-red-500': !editStore.isEqual }"
+    >
+      <div :class="{ 'h-[30rem] w-default': !isFullscreen }">
+        <VueDiff
+          :mode="'split'"
+          :theme="'dark'"
+          :language="'json'"
+          :prev="editStore.initialAsJson ?? ''"
+          :current="editStore.currentAsJson ?? ''"
+          :input-delay="10"
+        />
+      </div>
+    </ToolBoxPanel>
+  </UseFullscreen>
 </template>
 
 <script setup lang="ts">
 import { defineAsyncComponent } from 'vue';
 import 'vue-diff/dist/index.css';
+import UseFullscreen from '../../../../components/fullscreen/UseFullscreen.vue';
 import ToolBoxPanel from '../../toolBox/ToolBoxPanel.vue';
 import { useEditStore } from '../store/editStore';
 
