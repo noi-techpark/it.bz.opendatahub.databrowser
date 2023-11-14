c5237f527c9733975e3faa9099e955471855b1f9
gappc <gapp.christian@gmail.com>
2023-11-14 16:08:52 +0100

    tmp

--- databrowser/src/domain/api/pagination/mapper/arrayPagination.ts
+++ databrowser/src/domain/api/pagination/mapper/arrayPagination.ts
@@ -9,22 +9,9 @@ export const arrayPagination = <T = unknown>(
   { pageSize, currentPage }: { pageSize: number; currentPage: number }
 ): Pagination2 => {
   const totalItems = data.length;
-
-  // const pageSize =
-  //   routeQuery.pagesize != null
-  //     ? parseInt(stringifyParameter(routeQuery.pagesize), 10)
-  //     : 0;
-
-  // const currentPage =
-  //   routeQuery.pagenumber != null
-  //     ? parseInt(stringifyParameter(routeQuery.pagenumber), 10)
-  //     : 1;
-
   const pageCount = pageSize === 0 ? 1 : Math.floor(totalItems / pageSize) + 1;
   const hasPrevious = currentPage > 1;
   const hasNext = currentPage < pageCount;
-
-  // const { currentApiParams } = storeToRefs(useApiParameterStore());
 
   return {
     totalItems,
