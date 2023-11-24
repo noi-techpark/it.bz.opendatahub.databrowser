f6de3306a7e91f65dbf89151a16764a130360a57
gappc <gapp.christian@gmail.com>
2023-11-24 23:47:41 +0100

    Add support for non-clickable ButtonLink

--- databrowser/src/components/button/ButtonLink.vue
+++ databrowser/src/components/button/ButtonLink.vue
@@ -5,30 +5,42 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 -->
 
 <template>
-  <router-link :to="to" :class="className" button-link>
+  <component
+    :is="renderAsSpan ? 'span' : 'router-link'"
+    :to="to"
+    :class="[className, { 'pointer-events-none opacity-50': renderAsSpan }]"
+    button-link
+  >
     <slot></slot>
-  </router-link>
+  </component>
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, toRefs } from 'vue';
 import { RouteLocationRaw } from 'vue-router';
 import { sizeClass, variantClass } from './styles';
 import { Size, Tone, Variant } from './types';
 
 const props = withDefaults(
   defineProps<{
-    to: RouteLocationRaw;
+    to?: RouteLocationRaw;
     variant?: string;
     size?: string;
     tone?: string;
+    disabled?: boolean;
   }>(),
   {
+    to: undefined,
     variant: Variant.solid,
     size: Size.md,
     tone: Tone.primary,
+    disabled: false,
   }
 );
+
+const { to, disabled } = toRefs(props);
+
+const renderAsSpan = computed(() => to.value == null || disabled.value);
 
 const className = computed(() => {
   const variant = props.variant as Variant;
