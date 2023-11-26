8a4df3bd5d01d4cd2bb9c8834cf18a9669a1bf64
gappc <gapp.christian@gmail.com>
2023-11-26 15:26:01 +0100

    Don't show search in toolbox on tourism domain

--- databrowser/src/domain/datasets/tableView/toolBox/SearchAndFilterToolBoxPanel.vue
+++ databrowser/src/domain/datasets/tableView/toolBox/SearchAndFilterToolBoxPanel.vue
@@ -6,7 +6,7 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 
 <template>
   <ToolBoxPanel>
-    <ToolBoxCard>
+    <ToolBoxCard v-if="datasetDomain === 'tourism'">
       <ToolBoxCardHeader>
         <div class="flex items-center gap-2">
           {{
