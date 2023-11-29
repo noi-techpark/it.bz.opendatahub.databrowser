0f4820bb2281e08df829705c26208a4ac8d6eb32
gappc <gapp.christian@gmail.com>
2023-11-29 22:57:56 +0100

    Fix issue with hot module reload
    Fixing this makes the development / configuration experience better.

--- databrowser/src/domain/datasets/common/load/useSingleRecordLoadConfig.ts
+++ databrowser/src/domain/datasets/common/load/useSingleRecordLoadConfig.ts
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-import { storeToRefs } from 'pinia';
+import { toRefs } from 'vue';
 import { useDatasetInfoStore } from '../../../datasetConfig/store/datasetInfoStore';
 
 export const useSingleRecordLoadConfig = () => {
@@ -23,7 +23,13 @@ export const useSingleRecordLoadConfig = () => {
     isNewView,
     addRecordSupported,
     editRecordSupported,
-  } = storeToRefs(useDatasetInfoStore());
+    // Note: using toRefs here instead of storeToRefs because storeToRefs
+    // breaks hot module reload for the "view" property.
+    // This could be an issue if some store properties were not present
+    // during this invocation, but in our case we are sure that all properties
+    // are present (in the store itself they are refs / computed values).
+    // See also: https://github.com/vuejs/pinia/blob/v2/packages/pinia/src/storeToRefs.ts
+  } = toRefs(useDatasetInfoStore());
 
   return {
     fullPath,
--- databrowser/src/domain/datasets/tableView/load/useTableLoadConfig.ts
+++ databrowser/src/domain/datasets/tableView/load/useTableLoadConfig.ts
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-import { storeToRefs } from 'pinia';
+import { toRefs } from 'vue';
 import { useDatasetInfoStore } from '../../../datasetConfig/store/datasetInfoStore';
 
 export const useTableLoadConfig = () => {
@@ -16,7 +16,13 @@ export const useTableLoadConfig = () => {
     datasetPath,
     datasetQuery,
     editRecordSupported,
-  } = storeToRefs(useDatasetInfoStore());
+    // Note: using toRefs here instead of storeToRefs because storeToRefs
+    // breaks hot module reload for the "view" property.
+    // This could be an issue if some store properties were not present
+    // during this invocation, but in our case we are sure that all properties
+    // are present (in the store itself they are refs / computed values).
+    // See also: https://github.com/vuejs/pinia/blob/v2/packages/pinia/src/storeToRefs.ts
+  } = toRefs(useDatasetInfoStore());
 
   return {
     datasetDomain,
