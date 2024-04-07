e33c6b34ebe95fa9f7f73c8d8fabbb587c339fab
gappc <gapp.christian@gmail.com>
2024-04-07 16:02:23 +0200

    Add support for "disabled" property to PopoverCustomButton component

--- databrowser/src/components/popover/PopoverCustomButton.vue
+++ databrowser/src/components/popover/PopoverCustomButton.vue
@@ -5,11 +5,13 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 -->
 
 <template>
-  <PopoverButton v-slot="{ open, close }">
+  <PopoverButton v-slot="{ open, close }" :disabled="disabled">
     <slot :open="open" :close="close"></slot>
   </PopoverButton>
 </template>
 
 <script setup lang="ts">
 import { PopoverButton } from '@headlessui/vue';
+
+defineProps<{ disabled?: boolean }>();
 </script>
