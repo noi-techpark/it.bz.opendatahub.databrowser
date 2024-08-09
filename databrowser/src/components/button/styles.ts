278964a39cc90a74b6a39c9c9645fcc37beefde4
Matteo Biasi <matteobiasi@flashbeing.com>
2024-08-09 08:31:37 +0200

    feat: completed issues 526, 575 a and started 525

--- databrowser/src/components/button/styles.ts
+++ databrowser/src/components/button/styles.ts
@@ -23,6 +23,10 @@ export const variantClass: Record<Variant, Record<Tone, String>> = {
     [Tone.danger]:
       'focus-visible:bg-red-500/10 focus-visible:text-red-500 focus-visible:border-red-500 focus-visible:outline-none',
   },
+  [Variant.transparent]: {
+    [Tone.primary]:
+      'focus-visible:bg-green-500/10 focus-visible:text-green-500 focus-visible:border-green-500 focus-visible:outline-none',
+  },
 };
 
 export const sizeClass: Record<Size, String> = {
@@ -50,6 +54,9 @@ export const disabledClass: Record<Variant, Record<Tone, String>> = {
   [Variant.transparent]: {
     [Tone.primary]: 'bg-transparent text-grey-500 opacity-25',
     [Tone.danger]: 'bg-transparent text-red-500 opacity-25',
+  },
+  [Variant.transparent]: {
+    [Tone.primary]: 'bg-transparent text-grey-500 opacity-25',
   },
 };
 
