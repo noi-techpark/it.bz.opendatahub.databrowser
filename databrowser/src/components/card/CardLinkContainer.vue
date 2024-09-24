0d8942f43ac2e213a51d8053b511096c05986d4d
Matteo Biasi <matteobiasi@flashbeing.com>
2024-09-24 11:42:21 +0200

    fix: optimized CardLinkContainer display without link

--- databrowser/src/components/card/CardLinkContainer.vue
+++ databrowser/src/components/card/CardLinkContainer.vue
@@ -5,18 +5,19 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 -->
 
 <template>
-  <router-link
-    :to="to"
+  <component
+    :is="to ? 'router-link' : 'span'"
+    :to="to!"
     class="mb-3 rounded-t border border-transparent px-3 pt-3 no-underline hover:border-hint-info hover:bg-hint-info/10 lg:px-5 lg:pt-4"
   >
     <slot></slot>
-  </router-link>
+  </component>
 </template>
 
 <script setup lang="ts">
 import { RouteLocationRaw } from 'vue-router';
 
 defineProps<{
-  to: RouteLocationRaw;
+  to?: RouteLocationRaw;
 }>();
 </script>
