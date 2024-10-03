8d4901d5e8379ce40f6171bea88ad9ec158daf3c
gappc <gapp.christian@gmail.com>
2024-10-03 12:36:14 +0200

    Fix mobility pagination bug

--- databrowser/src/domain/datasets/pagination/mobilityPagination.ts
+++ databrowser/src/domain/datasets/pagination/mobilityPagination.ts
@@ -2,13 +2,27 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+import { minPageSize } from '../ui/tableView/defaultValues';
 import { Pagination, WithMobilityPagination } from './types';
 
 export const mobilityPagination = (
   data: WithMobilityPagination
 ): Pagination => {
-  // Arbitrary number, because there is no way to
-  // know the total number of items in mobility API
+  // If the number of records is < minPageSize, then show all records
+  if (data.data.length < minPageSize) {
+    return {
+      totalItems: data.data.length,
+      pageCount: 1,
+      pageSize: data.data.length,
+      currentPage: 1,
+      hasPrevious: false,
+      hasNext: false,
+      hasPagination: false,
+    };
+  }
+
+  // Set totalItems to an Infinity, because there is
+  // no way to know the total number of items in mobility API
   const totalItems = Infinity;
   const pageSize = data.limit;
   const currentPage = Math.floor(data.offset / pageSize) + 1;
