3e78c5719fc0049ba724290692f849a9cc8ac685
gappc <gapp.christian@gmail.com>
2025-01-12 17:12:05 +0100

    Add map view hint

---
+++ databrowser/src/domain/datasets/ui/mapView/MapViewHint.vue
@@ -0,0 +1,28 @@
+<!--
+SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
+
+SPDX-License-Identifier: AGPL-3.0-or-later
+-->
+
+<template>
+  <HintCustom
+    v-if="showMapViewNote"
+    type="calm"
+    title="Note!"
+    @close="hideHint"
+  >
+    {{ t('datasets.mapView.note') }}
+  </HintCustom>
+</template>
+
+<script setup lang="ts">
+import { useLocalStorage } from '@vueuse/core';
+import { useI18n } from 'vue-i18n';
+import HintCustom from '../../../../components/hint/HintCustom.vue';
+
+const { t } = useI18n();
+
+const showMapViewNote = useLocalStorage('showMapViewNote', true);
+
+const hideHint = () => (showMapViewNote.value = false);
+</script>
