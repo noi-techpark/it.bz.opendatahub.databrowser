7a81c650bb8656da5e0e0366ca2f17c516fba2d9
gappc <gapp.christian@gmail.com>
2025-02-20 16:19:25 +0100

    Simplify option computation for TagCell

--- databrowser/src/domain/cellComponents/components/cells/tagCell/TagCell.vue
+++ databrowser/src/domain/cellComponents/components/cells/tagCell/TagCell.vue
@@ -18,7 +18,7 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 </template>
 
 <script setup lang="ts">
-import { computed, ref, toRefs, useAttrs } from 'vue';
+import { computed, toRefs, useAttrs } from 'vue';
 import EditListCell from '../../utils/editList/EditListCell.vue';
 import TagTable from './TagTable.vue';
 import { useAttributeMapper } from './useAttributeMapper';
@@ -40,7 +40,7 @@ const { tags, sortByLabel, unique } = toRefs(props);
 
 const attrs = useAttrs();
 
-const { options } = useAttributeMapper(ref(attrs), sortByLabel);
+const { options } = useAttributeMapper(attrs, sortByLabel);
 
 const enableUniqueValue = computed(() => {
   // if unique is a boolean, return it
